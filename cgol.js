34b95e9e95dbd76d890928fb0b3c724587c86d7d
bme/fgonzalvez <bme/fgonzalvez>
2015-10-07 15:35:26 +0200

    An ugly-buggy solution

--- cgol.js
+++ cgol.js
@@ -28,7 +28,7 @@ var Cell = function(grid) {
 	this.domObject.className = options.cellClass;
   this.domObject.model = this;
 	this.alive = false;
-
+  this.neighbourgs = 0;
 	this.kill = function() {
 		this.domObject.style.backgroundColor = options.deadCellColor;
 		this.alive = false;	
@@ -57,27 +57,45 @@ var Grid = function(x, y) {
 	}
   this.getNeighbourgs = function(x, y) {
     var total = 0;
-    if(this.cellGrid[x - 1][y].alive) total++;
-    if(this.cellGrid[x + 1][y].alive) total++;
-    if(this.cellGrid[x][y - 1].alive) total++;
-    if(this.cellGrid[x][y + 1].alive) total++;
-    if(this.cellGrid[x + 1][y + 1].alive) total++;
-    if(this.cellGrid[x - 1][y - 1].alive) total++;
-    if(this.cellGrid[x + 1][y - 1].alive) total++;
-    if(this.cellGrid[x - 1][y + 1].alive) total++;
-
+    if(x!=0) {
+      if(this.cellGrid[x - 1][y].alive) total++;
+      if(y!=0) {
+        if(this.cellGrid[x - 1][y - 1].alive) total++;
+      }
+      if(y!=gridWidth-1){
+        if(this.cellGrid[x - 1][y + 1].alive) total++;
+        if(x!=gridHeight-1) {
+          if(this.cellGrid[x + 1][y + 1].alive) total++;
+        }
+      }
+    }
+    if(y!=0) {
+      if(this.cellGrid[x][y - 1].alive) total++;
+      if(x!=gridHeight-1) {
+        if(this.cellGrid[x + 1][y - 1].alive) total++;
+      }
+    }
+    if(x!=gridHeight-1) {
+      if(this.cellGrid[x + 1][y].alive) total++;
+    }
+    if(y!=gridWidth-1) {
+      if(this.cellGrid[x][y + 1].alive) total++;
+    }
     return total;
   }
 	this.simulate = function() {
     var n = 0;
 		for(var i = 0; i < x; i++) {
 			for(var j = 0; j < y; j++) {
-		  	this.cellGrid[i][j].alive ? this.cellGrid[i][j].kill() : this.cellGrid[i][j].revive(); 
-        n = getNeighbourgs(i,j);
-        if(this.cellGrid[i][j].alive && (1 > n || n > 4 )) this.cellGrid[i][j].kill();
-        else if(!this.cellGrid[i][j].alive && n==3) this.cellGrid[i][j].revive();
+        this.cellGrid[i][j].neighbourgs = this.getNeighbourgs(i,j);
 			}
 		}
+    for(var i = 0; i < x; i++) {
+      for(var j = 0; j < y; j++) {
+        if(this.cellGrid[i][j].alive && (2 > this.cellGrid[i][j].neighbourgs || this.cellGrid[i][j].neighbourgs > 4 )) this.cellGrid[i][j].kill();
+        else if(!this.cellGrid[i][j].alive && this.cellGrid[i][j].neighbourgs==3) this.cellGrid[i][j].revive();
+      }
+    }
 	}
 };
 
